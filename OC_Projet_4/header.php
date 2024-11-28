4baae17d0ff13d31464c2d9ce9545623bb8cae99
Suspiciusduck <bobbycit2@gmail.com>
2024-11-28 13:10:00 +0100

    bdd init

--- OC_Projet_4/header.php
+++ OC_Projet_4/header.php
@@ -14,7 +14,6 @@
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="ajouter.php">Ajouter une œuvre</a></li>
         </ul>
     </nav>
 </header>
--- OC_Projet_4/oeuvre.php
+++ OC_Projet_4/oeuvre.php
@@ -1,6 +1,5 @@
 <?php
-    require 'header.php';
-    require 'oeuvres.php';
+    include 'header.php';
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
@@ -8,15 +7,6 @@
     }
 
     $oeuvre = null;
-
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
     if(is_null($oeuvre)) {
